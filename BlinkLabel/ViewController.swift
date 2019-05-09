ac20c54c40e505f3ad0d7f3002e6973589ae1f00
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-05-09 09:07:57 +0600

    Initial Commit

---
+++ BlinkLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlinkLabel
+//
+//  Created by Nayem BJIT on 5/9/19.
+//  Copyright © 2019 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
